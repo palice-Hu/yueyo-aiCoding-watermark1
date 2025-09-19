b32f956d275c3f5ac3539308ff6d7d1d4eadb4cd
palice-Hu <1634297782@qq.com>
2025-09-19 10:00:11 +0800

    初始化项目目录结构和基本配置文件

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>照片水印添加工具</h1>
+    <div>选择文件夹并添加水印</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// 主应用程序组件
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
